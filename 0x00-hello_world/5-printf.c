cf69861918b1124fcd0c6d63231a68ab5e89837c
Oderhohwo <osquare4real2007@gmail.com>
2023-08-10 17:20:28 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Main entry
+ * A program that print out a grammar sentence
+ *
+ * Return: 0 (sucess)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
