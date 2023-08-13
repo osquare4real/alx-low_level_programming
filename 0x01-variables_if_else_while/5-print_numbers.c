8960a21b97cc39a0a195aa79fe8762777aba9f48
Oderhohwo <osquare4real2007@gmail.com>
2023-08-13 15:36:11 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Code that print zero to ten
+ *
+ * Return: 0 (Success)
+ *
+ */
+int main(void)
+
+{
+	int h;
+
+for (h = 0; h < 10; h++)
+printf("%d", h);
+printf("\n");
+
+return (0);
+}
