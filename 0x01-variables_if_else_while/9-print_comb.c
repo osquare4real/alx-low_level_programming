1556a4aa6df46147568a7f0f71e26d6ce8e106de
Oderhohwo <osquare4real2007@gmail.com>
2023-08-14 19:24:22 -0700

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,17 +10,17 @@ int main(void)
 {
 	int num;
 
-	for (num = 0 ;  num < 10 ; num++)
+	for (num = 0; num < 10; num++)
 	{
-		if (num == 9)
-			putchar(num + '0');
-		else
+		putchar(num + '0');
+
+		if (num != 9)
 		{
-			putchar(num + '0');
 			putchar(',');
 			putchar(' ');
 		}
 	}
-
+putchar('\n')
+	;
 return (0);
 }
