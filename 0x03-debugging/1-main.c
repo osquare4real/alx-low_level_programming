ae7fa54b2767893e7b95e0322d5ec98bff36abac
Oderhohwo <osquare4real2007@gmail.com>
2023-08-17 05:53:57 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 /**
+*the header to determine the prinf command is not included
 * main - causes an infinite loop
 * Return: 0
 */
@@ -18,7 +19,7 @@ int main(void)
 		putchar(i);
         }
 
-	rintf("Infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
