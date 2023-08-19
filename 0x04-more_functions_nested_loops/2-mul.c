99d30b2f333229388d5c2327e4a8a51033187c00
Oderhohwo <osquare4real2007@gmail.com>
2023-08-18 18:01:15 -0700

    All 0x04More function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - Code to  multiply two number
+ * @a: first int
+ * @b: second int
+ * Return: addition of the multiplication
+ */
+int mul(int a, int b)
+
+
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Code to  for a digit (0 through 9).
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = 0; c < 9; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
